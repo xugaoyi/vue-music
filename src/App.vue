e90b7a29b3e4fcfc730f35fe7cf1b4907271fead
xugaoyi <894072666@qq.com>
2019-06-17 17:19:07 +0800

    2

--- src/App.vue
+++ src/App.vue
@@ -1,16 +1,18 @@
 <template>
   <div id="app">
-    hello world
+    <m-header></m-header>
   </div>
 </template>
 
 <script type="text/ecmascript-6">
-
+import MHeader from '@/components/m-header/m-header'
+export default {
+  components: {
+    MHeader
+  }
+}
 </script>
 
 <style scoped lang="stylus" rel="stylesheet/stylus">
-  @import "~common/stylus/variable"
 
-  #app
-    color: $color-theme
 </style>
--- src/main.js
+++ src/main.js
@@ -1,8 +1,12 @@
+import 'babel-polyfill'
 import Vue from 'vue'
 import App from './App'
 // import router from './router'
+import fastclick from 'fastclick'
 
 import 'common/stylus/index.styl'
+
+fastclick.attch(document.body)
 
 /* eslint-disable no-new */
 new Vue({
