0507e51e4f8ef254dd89a42bb4b82589e50dbf95
xugaoyi <894072666@qq.com>
2019-06-18 17:03:03 +0800

    实现图片滚动

--- src/App.vue
+++ src/App.vue
@@ -1,14 +1,19 @@
 <template>
   <div id="app">
     <m-header></m-header>
+    <tad></tad>
+    <router-view></router-view>
   </div>
 </template>
 
 <script type="text/ecmascript-6">
-import MHeader from '@/components/m-header/m-header'
+import MHeader from './components/m-header/m-header'
+import Tad from './components/tab/tab'
+
 export default {
   components: {
-    MHeader
+    MHeader,
+    Tad
   }
 }
 </script>
