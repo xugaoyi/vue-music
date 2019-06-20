75d678a68e6249d949b3238e4e0df9b350bb8914
xugaoyi <894072666@qq.com>
2019-06-20 13:05:54 +0800

    完善歌单推荐

--- src/base/scroll/scroll.vue
+++ src/base/scroll/scroll.vue
@@ -1,60 +1,61 @@
 <template>
+   <!-- scroll滑动页面的抽象组件 -->
   <div ref="wrapper">
     <slot></slot>
   </div>
 </template>
 
 <script type="text/ecmascript-6">
-  import BScroll from 'better-scroll'
+import BScroll from 'better-scroll'
 
-  export default {
-    props: {
-      probeType: {
-        type: Number,
-        default: 1
-      },
-      click: {
-        type: Boolean,
-        default: true
-      },
-      data: {
-        type: Array,
-        default: null
-      }
+export default {
+  props: {
+    probeType: {
+      type: Number,
+      default: 1
     },
-    mounted() {
+    click: {
+      type: Boolean,
+      default: true
+    },
+    data: {
+      type: Array,
+      default: null
+    }
+  },
+  mounted() {
+    setTimeout(() => {
+      this._initScroll()
+    }, 20)
+  },
+  methods: {
+    _initScroll() {
+      if (!this.$refs.wrapper) {
+        return
+      }
+      this.scroll = new BScroll(this.$refs.wrapper, {
+        probeType: this.probeType,
+        click: this.click
+      })
+    },
+    enable() {
+      this.scroll && this.scroll.enable()
+    },
+    disable() {
+      this.scroll && this.scroll.disable()
+    },
+    refresh() {
+      this.scroll && this.scroll.refresh()
+    }
+  },
+  watch: {
+    data() {
       setTimeout(() => {
-        this._initScroll()
+        this.refresh()
       }, 20)
-    },
-    methods: {
-      _initScroll() {
-        if (!this.$refs.wrapper) {
-          return
-        }
-        this.scroll = new BScroll(this.$refs.wrapper, {
-          probeType: this.probeType,
-          click: this.click
-        })
-      },
-      enable() {
-        this.scroll && this.scroll.enable()
-      },
-      disable() {
-        this.scroll && this.scroll.disable()
-      },
-      refresh() {
-        this.scroll && this.scroll.refresh()
-      }
-    },
-    watch: {
-      data() {
-        setTimeout(() => {
-          this.refresh()
-        }, 20)
-      }
     }
   }
+}
 
 </script>
 
--- src/components/recommend/recommend.vue
+++ src/components/recommend/recommend.vue
@@ -1,34 +1,37 @@
 <template>
   <div class="recommend">
-    <div class="recommend-content">
-      <div v-if="recommends.length" class="slider-wrapper">
-        <slider>
-          <div v-for="(item,index) in recommends" :key="index">
-            <a :href="item.linkUrl">
-              <img :src="item.picUrl">
-            </a>
-          </div>
-        </slider>
-      </div>
-      <div class="recommend-list">
-        <h1 class="list-title">热门歌单推荐</h1>
-        <ul>
-          <li v-for="(item,index) in discList" :key="index" class="item">
-            <div class="icon">
-              <img width="60px" height="60px" :src="item.imgurl">
+    <scroll ref="scroll" class="recommend-content" :data="discList">
+      <div>
+        <div v-if="recommends.length" class="slider-wrapper">
+          <slider>
+            <div v-for="(item,index) in recommends" :key="index">
+              <a :href="item.linkUrl">
+                <img @load="loadImage" :src="item.picUrl">
+              </a>
             </div>
-            <div class="text">
-              <h2 class="name" v-html="item.creator.name"></h2>
-              <p class="desc" v-html="item.dissname"></p>
-            </div>
-          </li>
-        </ul>
+          </slider>
+        </div>
+        <div class="recommend-list">
+          <h1 class="list-title">热门歌单推荐</h1>
+          <ul>
+            <li v-for="(item,index) in discList" :key="index" class="item">
+              <div class="icon">
+                <img width="60px" height="60px" v-lazy="item.imgurl">
+              </div>
+              <div class="text">
+                <h2 class="name" v-html="item.creator.name"></h2>
+                <p class="desc" v-html="item.dissname"></p>
+              </div>
+            </li>
+          </ul>
+        </div>
       </div>
-    </div>
+    </scroll>
   </div>
 </template>
 
 <script type="text/ecmascript-6">
+import Scroll from '@/base/scroll/scroll'
 import Slider from '@/base/slider/slider'
 import { getRecommend, getDiscList } from '@/api/recommend'
 import { ERR_OK } from '@/api/config'
@@ -58,10 +61,17 @@ export default {
           this.discList = res.data.list
         }
       })
+    },
+    loadImage() {
+      if (!this.checkLoaded) {
+        this.$refs.scroll.refresh()
+        this.checkLoaded = true
+      }
     }
   },
   components: {
-    Slider
+    Slider,
+    Scroll
   }
 }
 </script>
