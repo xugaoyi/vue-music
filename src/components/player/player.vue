142676d38a4ee030d5bb91647bf4e8b0ea3a2ed7
xugaoyi <894072666@qq.com>
2019-07-31 18:20:44 +0800

    播放器

--- src/components/player/player.vue
+++ src/components/player/player.vue
@@ -2,111 +2,257 @@
   <div class="player" v-show="playlist.length>0">
 
     <!-- 标准播放器 s-->
-    <div class="normal-player" v-show="fullScreen">
+    <transition name="normal"
+      @enter="enter"
+      @after-enter="afterEnter"
+      @leave="leave"
+      @after-leave="afterLeave"
+    > <!-- vue的动画钩子函数：@enter @after-enter @leave @after-leave-->
+      <div class="normal-player" v-show="fullScreen">
 
-      <!-- 背景图 s-->
-      <div class="background">
-        <img width="100%" height="100%" :src="currentSong.image">
-      </div>
-      <!-- 背景图 e-->
-
-      <!-- top s-->
-      <div class="top">
-        <div class="back">
-          <i class="icon-back"></i>
+        <!-- 背景图 s-->
+        <div class="background">
+          <img width="100%" height="100%" :src="currentSong.image">
         </div>
-        <h1 class="title" v-html="currentSong.name"></h1>
-        <h2 class="subtitle" v-html="currentSong.singer"></h2>
-      </div>
-      <!-- top e-->
+        <!-- 背景图 e-->
 
-      <!-- middle s-->
-      <div class="middle">
-        <div class="middle-l" ref="middleL">
-          <div class="cd-wrapper" ref="cdWrapper">
-            <div class="cd">
-              <img class="image" :src="currentSong.image">
+        <!-- top s-->
+        <div class="top">
+          <div class="back" @click="back">
+            <i class="icon-back"></i>
+          </div>
+          <h1 class="title" v-html="currentSong.name"></h1>
+          <h2 class="subtitle" v-html="currentSong.singer"></h2>
+        </div>
+        <!-- top e-->
+
+        <!-- middle s-->
+        <div class="middle">
+          <div class="middle-l" ref="middleL">
+            <div class="cd-wrapper" ref="cdWrapper">
+              <div class="cd" :class="cdCls">
+                <img class="image" :src="currentSong.image">
+              </div>
+            </div>
+            <div class="playing-lyric-wrapper">
+              <div class="playing-lyric"></div>
             </div>
           </div>
-          <div class="playing-lyric-wrapper">
-            <div class="playing-lyric"></div>
-          </div>
+          <!-- <scroll class="middle-r" ref="lyricList">
+            <div class="lyric-wrapper">
+              <div>
+                <p ref="lyricLine" ></p>
+              </div>
+            </div>
+          </scroll> -->
         </div>
-        <!-- <scroll class="middle-r" ref="lyricList">
-          <div class="lyric-wrapper">
-            <div>
-              <p ref="lyricLine" ></p>
+        <!-- middle e-->
+
+        <!-- bottom s-->
+        <div class="bottom">
+          <div class="dot-wrapper">
+            <span class="dot"></span>
+            <span class="dot"></span>
+          </div>
+          <div class="progress-wrapper"><!-- 播放进度条 -->
+            <span class="time time-l"></span>
+            <div class="progress-bar-wrapper">
+            </div>
+            <span class="time time-r"></span>
+          </div>
+          <div class="operators"><!-- 操作按钮 -->
+            <div class="icon i-left">
+              <i class="icon-sequence"></i>
+            </div>
+            <div class="icon i-left">
+              <i @click="prev" class="icon-prev"></i>
+            </div>
+            <div class="icon i-center">
+              <i @click="togglePlaying" :class="playIcon"></i>
+            </div>
+            <div class="icon i-right">
+              <i @click="next" class="icon-next"></i>
+            </div>
+            <div class="icon i-right">
+              <i class="icon icon-not-favorite"></i>
             </div>
           </div>
-        </scroll> -->
+        </div>
+        <!-- bottom e-->
       </div>
-      <!-- middle e-->
-
-      <!-- bottom s-->
-      <div class="bottom">
-        <div class="dot-wrapper">
-          <span class="dot"></span>
-          <span class="dot"></span>
-        </div>
-        <div class="progress-wrapper"><!-- 播放进度条 -->
-          <span class="time time-l"></span>
-          <div class="progress-bar-wrapper">
-          </div>
-          <span class="time time-r"></span>
-        </div>
-        <div class="operators"><!-- 操作按钮 -->
-          <div class="icon i-left">
-            <i class="icon-sequence"></i>
-          </div>
-          <div class="icon i-left">
-            <i class="icon-prev"></i>
-          </div>
-          <div class="icon i-center">
-            <i class="icon-play"></i>
-          </div>
-          <div class="icon i-right">
-            <i class="icon-next"></i>
-          </div>
-          <div class="icon i-right">
-            <i class="icon icon-not-favorite"></i>
-          </div>
-        </div>
-      </div>
-      <!-- bottom e-->
-    </div>
+    </transition>
     <!-- 标准播放器 e-->
 
     <!-- 迷你播放器 s-->
-    <div class="mini-player" v-show="!fullScreen">
-      <div class="icon">
-        <img width="40" height="40" :src="currentSong.image">
+    <transition name="mini">
+      <div class="mini-player" v-show="!fullScreen" @click="open">
+        <div class="icon">
+          <img :class="cdCls" width="40" height="40" :src="currentSong.image">
+        </div>
+        <div class="text">
+          <h2 class="name" v-html="currentSong.name"></h2>
+          <p class="desc" v-html="currentSong.singer"></p>
+        </div>
+        <div class="control">
+          <i @click.stop="togglePlaying" :class="miniIcon"></i> <!-- .stop 阻止冒泡事件 防止触发父级的open事件 -->
+        </div>
+        <div class="control">
+          <i class="icon-playlist"></i>
+        </div>
       </div>
-      <div class="text">
-        <h2 class="name" v-html="currentSong.name"></h2>
-        <p class="desc" v-html="currentSong.singer"></p>
-      </div>
-      <div class="control">
-      </div>
-      <div class="control">
-        <i class="icon-playlist"></i>
-      </div>
-    </div>
+    </transition>
     <!-- 迷你播放器 e-->
+
+    <!-- html5播放音频 s-->
+    <audio ref="audio" :src="currentSong.url" @canplay="ready" @error="error"></audio>
+    <!-- html5播放音频 e-->
   </div>
 </template>
 
 <script type="text/ecmascript-6">
-import { mapGetters } from 'vuex' // 获取vuex的数据 语法糖
+import { mapGetters, mapMutations } from 'vuex' // 获取/设置vuex的数据 语法糖
+import animations from 'create-keyframe-animation' // 动画库依赖https://github.com/HenrikJoreteg/create-keyframe-animation
+import { prefixStyle } from '@/common/js/dom' // js操作css3样式前缀的封装
+
+const transform = prefixStyle('transform')
 
 export default {
+  data() {
+    return {
+      songReady: false
+    }
+  },
   computed: {
-    ...mapGetters([ // 获取到相应数据
-      'fullScreen',
-      'playlist',
-      'currentSong'
+    cdCls() {
+      return this.playing ? 'play' : 'play pause'
+    },
+    playIcon() {
+      return this.playing ? 'icon-pause' : 'icon-play'
+    },
+    miniIcon() {
+      return this.playing ? 'icon-pause-mini' : 'icon-play-mini'
+    },
+    ...mapGetters([ // 获取vuex数据
+      'fullScreen', // 播放器展开收起(大播放与小播放器)
+      'playlist', // 歌曲播放列表
+      'currentSong', // 当前播放歌曲数据
+      'playing', // 是否播放
+      'currentIndex' // 当前播放索引
     ])
   },
   methods: {
+    back() { // 收起大播放器
+      this.setFullScreen(false)
+    },
+    open() { // 展开大播放器
+      this.setFullScreen(true)
+    },
+    enter(el, done) { // 从小cd图片到大cd图片的动画 参数done：动画执行完的回调，并执行afterEnter方法
+      const {x, y, scale} = this._getPosAndScale()
+      let animation = { // 带回弹动画
+        0: {
+          transform: `translate3d(${x}px,${y}px,0) scale(${scale})`
+        },
+        60: {
+          transform: `translate3d(0,0,0) scale(1.1)`
+        },
+        100: {
+          transform: `translate3d(0,0,0) scale(1)`
+        }
+      }
+      animations.registerAnimation({ // 注册动画
+        name: 'move',
+        animation,
+        presets: {
+          duration: 400,
+          easing: 'linear'
+        }
+      })
+      animations.runAnimation(this.$refs.cdWrapper, 'move', done) // 运行动画 参数done：动画执行完执行的回调
+    },
+    afterEnter() { // 放大动画结束
+      animations.unregisterAnimation('move') // 移除动画
+      this.$refs.cdWrapper.style.animation = ''
+    },
+    leave(el, done) { // 从大cd图片到小cd图片的动画
+      this.$refs.cdWrapper.style.transition = 'all 0.4s'
+      const {x, y, scale} = this._getPosAndScale()
+      this.$refs.cdWrapper.style[transform] = `translate3d(${x}px,${y}px,0) scale(${scale})`
+      this.$refs.cdWrapper.addEventListener('transitionend', done) // transitionend事件在CSS完成过渡后触发，触发回调函数done
+    },
+    afterLeave() { // 缩小动画结束
+      this.$refs.cdWrapper.style.transition = ''
+      this.$refs.cdWrapper.style[transform] = ''
+    },
+    togglePlaying() { // 播放暂停
+      if (!this.songReady) { // 防止快速点击时报错
+        return
+      }
+      this.setPlayingState(!this.playing)
+    },
+    prev() { // 上一曲
+      if (!this.songReady) { // 防止快速点击时报错
+        return
+      }
+      let index = this.currentIndex - 1
+      if (index === -1) {
+        index = this.playlist.length - 1
+      }
+      this.setCurrentIndex(index)
+      if (!this.playing) {
+        this.togglePlaying()
+      }
+      this.songReady = false
+    },
+    next() { // 下一曲
+      if (!this.songReady) { // 防止快速点击时报错
+        return
+      }
+      let index = this.currentIndex + 1
+      if (index === this.playlist.length) {
+        index = 0
+      }
+      this.setCurrentIndex(index)
+      if (!this.playing) {
+        this.togglePlaying()
+      }
+      this.songReady = false
+    },
+    ready() {
+      this.songReady = true // 防止快速点击时报错
+    },
+    error() {
+
+    },
+    _getPosAndScale() { // 获取大小唱片图片的位置 (计算两个图片中心点的位置)
+      const targetWidth = 40
+      const paddingLeft = 40
+      const paddingBottom = 30
+      const paddingTop = 80
+      const width = window.innerWidth * 0.8
+      const scale = targetWidth / width
+      const x = -(window.innerWidth / 2 - paddingLeft)
+      const y = window.innerHeight - paddingTop - width / 2 - paddingBottom
+      return {x, y, scale}
+    },
+    ...mapMutations({ // 映射 提交mutations
+      setFullScreen: 'SET_FULL_SCREEN',
+      setPlayingState: 'SET_PLAYING_STATE',
+      setCurrentIndex: 'SET_CURRENT_INDEX'
+    })
+  },
+  watch: {
+    currentSong() {
+      this.$nextTick(() => { // DOM 更新结束后
+        this.$refs.audio.play() // 播放音频
+      })
+    },
+    playing(newPlaying) { // 执行播放暂停音频
+      const audio = this.$refs.audio
+      this.$nextTick(() => {
+        newPlaying ? audio.play() : audio.pause()
+      })
+    }
   }
 }
 </script>
